99cb0fccafb281b73d362c5c69c1481bafb9a50f
Macgyver Martins <macgyver.martins@gmail.com>
2017-10-05 02:10:10 -0400

    Add model generator

---
+++ commands/generate.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const program = require('commander');
+const nodePlop = require('node-plop');
+const plop = nodePlop(path.join(__dirname, '../plopfile.js'));
+
+module.exports = function helloCommand(program) {
+
+  program
+    .command('generate <snippet> <name> [propsList...]')
+    .alias('g')
+    .usage('<snippet> <name> [propsList...]')
+    .action(function(snippet, name, propsList){
+      if (snippet !== 'model') {
+        return program.errorMessage(`Unknow snippet '${snippet}'. \n Please, run 'gexpress g --help'`);
+      }
+
+      const modelGenerator = plop.getGenerator(snippet);
+      modelGenerator.runActions({name: name}).then(function (a, b) {
+        return program.successMessage(`${name} ${snippet} created!`);
+      });
+    })
+    .parse(process.argv);
+
+};
---
+++ commands/hello.js
@@ -0,0 +1,14 @@
+module.exports = function helloCommand(program) {
+  'use strict';
+
+  program
+    .command('hello <name>')
+    .description('Say hello to <name>')
+    .action(function(name, command) {
+
+      console.log('Hello ' + name);
+      program.log(name);
+
+    });
+
+};

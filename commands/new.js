7bf2cbd8de01cb6fec707ab90d28a78209137586
Macgyver Martins <macgyver.martins@gmail.com>
2017-10-05 21:44:17 -0400

    Add app.js

---
+++ commands/new.js
@@ -0,0 +1,90 @@
+const path = require("path");
+const ejs = require('ejs');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const program = require('commander');
+const nodePlop = require('node-plop');
+const _ = require('lodash');
+var s = require("underscore.string");
+
+const MODE_0666 = parseInt('0666', 8)
+const MODE_0755 = parseInt('0755', 8)
+
+function launchedFromCmd () {
+  return process.platform === 'win32' &&
+    process.env._ === undefined
+}
+
+/**
+ * Load template file.
+ */
+
+function loadTemplate (name) {
+  var contents = fs.readFileSync(path.join(__dirname, '..', 'templates', (name + '.ejs')), 'utf-8')
+  var locals = Object.create(null)
+
+  function render () {
+    return ejs.render(contents, locals)
+  }
+
+  return {
+    locals: locals,
+    render: render
+  }
+}
+
+/**
+ * echo str > path.
+ *
+ * @param {String} path
+ * @param {String} str
+ */
+
+function write (path, str, mode) {
+  fs.writeFileSync(path, str, { mode: mode || MODE_0666 })
+  console.log('   \x1b[36mcreate\x1b[0m : ' + path)
+}
+
+function complete (name, path) {
+  var prompt = launchedFromCmd() ? '>' : '$'
+
+  console.log()
+  console.log('   install dependencies:')
+  console.log('     %s cd %s && npm install', prompt, path)
+  console.log()
+  console.log('   run the app:')
+
+  if (launchedFromCmd()) {
+    console.log('     %s SET DEBUG=%s:* & npm start', prompt, name)
+  } else {
+    console.log('     %s DEBUG=%s:* npm start', prompt, name)
+  }
+
+  console.log()
+}
+
+module.exports = function newCommand(program) {
+
+  program
+    .command('new')
+    .usage('E isso ai ')
+    .action(function(snippet, name, propsList){
+
+      //var destinationPath = program.args.shift() || '.'
+      var destinationPath = process.cwd()
+      console.log(destinationPath);
+
+      var app = loadTemplate('js/app.js');
+
+      app.locals.modules = Object.create(null);
+      app.locals.view = Object.create(null);
+      app.locals.uses = [];
+
+      app.locals.modules.lessMiddleware = 'less-middleware'
+      write(destinationPath + '/app.js', app.render())
+      complete('hello-app', destinationPath)
+
+    })
+    .parse(process.argv);
+
+};
--- index.js
+++ index.js
@@ -70,34 +70,3 @@ program.on('*', function() {
 
 // Process Commands
 program.parse(process.argv);
-
-//const path = require("path");
-//const program = require('commander');
-//const nodePlop = require('node-plop');
-//const plop = nodePlop(path.join(__dirname, 'plopfile.js'));
-
-//program
-  //.arguments('<snippet>')
-  //.option('-n, --name <model name>', 'The model name meu')
-  //.action(function(snippet, options){
-    //const modelGenerator = plop.getGenerator(snippet);
-    //modelGenerator.runActions({name: 'macgyver'}).then(function (results) {
-      //console.log(__dirname);
-      //console.log(`Current directory: ${process.cwd()}`);
-      //console.log('res', results);
-//// do something after the actions have run
-    //});
-  //})
-  //.parse(process.argv);
-
-//// load an instance of plop from a plopfile
-////const plop = nodePlop('./plopfile.js');
-//// get a generator by name
-
-//// run all the generator actions using the data specified
-////modelGenerator.runActions().then(function (results) {
-////console.log(__dirname);
-////console.log(`Current directory: ${process.cwd()}`);
-////console.log('res', results);
-////// do something after the actions have run
-////});
